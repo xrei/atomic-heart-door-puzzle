fa38131bf502cae42149e517d0932f9f8a8825b4
xrei <romanrei.dev@gmail.com>
2023-03-05 20:27:20 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), babel({exclude: 'node_modules/**', babelHelpers: 'bundled'})],
-
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@app': resolve(__dirname, './src'),
