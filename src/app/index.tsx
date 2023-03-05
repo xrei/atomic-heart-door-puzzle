fb2c03030bc67d57feb7d8364bb9ec1f134005cb
xrei <romanrei.dev@gmail.com>
2023-03-06 02:45:34 +0700

    improve mobile layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import {Game} from '@app/puzzle'
 
 export const App = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-stone-600">
+    <div className="flex flex-col flex-1 bg-stone-600">
       <div className="flex flex-col items-center justify-center flex-1">
         <Game />
       </div>
